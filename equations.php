2b727ca35f6302bd8416b0dc49010dc8d76d41ee
Neil Jackson <neil@autoscaninc.com>
2016-07-09 19:00:04 -0700

    deleted test2.txt

---
+++ equations.php
@@ -0,0 +1,15 @@
+<?php
+
+//suppress error reporting for warnings, because this script generates a lot of the warnings: 'creating default object from empty value'.
+error_reporting(error_reporting() & ~E_WARNING);
+
+setParameterSetId("4d8f1872637445bebb11d60ae753d219");
+
+$inch = 25.4;
+$mm = 1;
+
+$degree = 1;
+$radian = 180/pi();
+
+
+?>
---
+++ makefile
@@ -0,0 +1,2 @@
+default:
+	php design_framework/equationProcessor.php --source="equations.php" --jsonOutputFile="solidworks_model/parameters-<id>.json"
